ae5109bf4bdf92343af975d2c2c0ccf2044aeaea
Shaafiee <shaafiee@gmail.com>
2021-01-23 01:46:09 +0800

    high resolution clock

--- ConsoleApplication2/fizzbuzz.cpp
+++ ConsoleApplication2/fizzbuzz.cpp
@@ -1,21 +1,24 @@
 
 
 #include <iostream>
-#include <sys/timeb.h>
+//#include <sys/timeb.h>
+#include <chrono>
 
 int main()
 {
-	int startTime, endTime, fizz, buzz, fizzbuzz;
-	timeb timeVar;
+	//int startTime, endTime;
+	int fizz, buzz, fizzbuzz;
+	//timeb timeVar;
 
 	fizz = 0;
 	buzz = 0;
 	fizzbuzz = 0;
 
-	ftime(&timeVar);
-	startTime = timeVar.millitm + (timeVar.time & 0xfffff) * 1000;
+	//ftime(&timeVar);
+	//startTime = timeVar.millitm + (timeVar.time & 0xfffff) * 1000;
+	auto startTime = std::chrono::high_resolution_clock::now();
 
-	for (int i = 1; i <= 1000000000; i++) {
+	for (int i = 1; i <= 100; i++) {
 		if (i % 3 == 0) {
 			if (i % 5 == 0) {
 				fizzbuzz++;
@@ -32,12 +35,43 @@ int main()
 				buzz++;
 			}
 		}
+
+		/*
+		if (i % 3 == 0 && i % 5 == 0) {
+			fizzbuzz++;
+		}
+		else {
+			if (i % 3 == 0) {
+				fizz++;
+				continue;
+			}
+			if (i % 5 == 0) {
+				buzz++;
+			}
+		}
+		*/
+
+		/*
+		if (i % 3 == 0) {
+			fizz++;
+		}
+		else if (i % 5 == 0) {
+			buzz++;
+		}
+		else if (i % 3 == 0 && i % 5 == 0) {
+			fizzbuzz++;
+		}
+		*/
 	}
 
-	ftime(&timeVar);
-	endTime = timeVar.millitm + (timeVar.time & 0xfffff) * 1000;
+	//ftime(&timeVar);
+	//endTime = timeVar.millitm + (timeVar.time & 0xfffff) * 1000;
 
-	std::cout << "fizz : " << fizz << ", buzz : " << buzz << ", fizzbuzz : " << fizzbuzz << " -- duration : " << (endTime - startTime) << "ms";
+	auto endTime = std::chrono::high_resolution_clock::now();
+	auto totalTime = endTime - startTime;
+
+	//std::cout << "fizz : " << fizz << ", buzz : " << buzz << ", fizzbuzz : " << fizzbuzz << " -- duration : " << (endTime - startTime) << "ms";
+	std::cout << "fizz : " << fizz << ", buzz : " << buzz << ", fizzbuzz : " << fizzbuzz << " -- duration : " << (totalTime / std::chrono::nanoseconds(1)) << "nanoseconds";
 
 	return 0;
 }
